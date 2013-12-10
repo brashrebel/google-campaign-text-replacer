c9ffd0fdfe56d5131044de93d1ec847506e727a9
Kyle <kyle@realbigmarketing.com>
2013-12-09 23:03:18 -0500

    Admin instructions
    Extended the admin page to have great instructions.

--- admin/admin.php
+++ admin/admin.php
@@ -16,9 +16,74 @@ function gctr_body_fcn() {
 		<br>
 	</div>
 	<h2>Google Campaign Text Replacer</h2>
-<p>
-	This plugin will be great.
-</p>
+<div class="updated">
+<b>Note:</b> All shortcodes on this page have spaces before and after square brackets. That is so they don't execute on this page.
+Remove the spaces in the shortcodes to make them work on real content.
+</div>
+	<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row">
+	    			<h3>Source</h3>
+	    		</th>
+	    		<td>
+	    			<b>Shortcode:</b> [ gctr_source ]<br/>
+	    			If the utm_source parameter is defined in the URL then the value will replace the text wrapped in the shortcode.
+	    			<br/><i>Example content:</i> <code>This visitor came from [ gctr_source ]Bing[ /gctr_source ]</code>
+	    			<br/><i>Example URL:</i> <code>http://realbigmarketing.com/?utm_source=Google</code>
+	    			<br/><i>End result:</i> <code>This visitor came from Google</code>
+        		</td>
+        	</tr>
+			<tr valign="top">
+				<th scope="row">
+	    			<h3>Medium</h3>
+	    		</th>
+	    		<td>
+	    			<b>Shortcode:</b> [ gctr_medium ]<br/>
+	    			If the utm_medium parameter is defined in the URL then the value will replace the text wrapped in the shortcode.
+	    			<br/><i>Example content:</i> <code>This visitor clicked on an [ gctr_medium ]E-mail link[ /gctr_medium ]</code>
+	    			<br/><i>Example URL:</i> <code>http://realbigmarketing.com/?utm_medium=Banner%20Ad</code>
+	    			<br/><i>End result:</i> <code>This visitor clicked on an Banner Ad</code>
+        		</td>
+        	</tr>
+			<tr valign="top">
+				<th scope="row">
+	    			<h3>Term</h3>
+	    		</th>
+	    		<td>
+	    			<b>Shortcode:</b> [ gctr_term ]<br/>
+	    			If the utm_term parameter is defined in the URL then the value will replace the text wrapped in the shortcode.
+	    			<br/><i>Example content:</i> <code>This visitor was seeking [ gctr_term ]the meaning of life[ /gctr_term ]</code>
+	    			<br/><i>Example URL:</i> <code>http://realbigmarketing.com/?utm_term=a%20good%20time</code>
+	    			<br/><i>End result:</i> <code>This visitor was seeking a good time</code>
+        		</td>
+        	</tr>
+			<tr valign="top">
+				<th scope="row">
+	    			<h3>Content</h3>
+	    		</th>
+	    		<td>
+	    			<b>Shortcode:</b> [ gctr_content ]<br/>
+	    			If the utm_content parameter is defined in the URL then the value will replace the text wrapped in the shortcode.
+	    			<br/><i>Example content:</i> <code>This visitor clicked on the [ gctr_content ]huge sale[ /gctr_content ] ad</code>
+	    			<br/><i>Example URL:</i> <code>http://realbigmarketing.com/?utm_content=black%20Friday</code>
+	    			<br/><i>End result:</i> <code>This visitor clicked on the black Friday ad</code>
+        		</td>
+        	</tr>
+			<tr valign="top">
+				<th scope="row">
+	    			<h3>Campaign name</h3>
+	    		</th>
+	    		<td>
+	    			<b>Shortcode:</b> [ gctr_name ]<br/>
+	    			If the utm_name parameter is defined in the URL then the value will replace the text wrapped in the shortcode.
+	    			<br/><i>Example content:</i> <code>This visitor came from our [ gctr_name ]plain Jane[ /gctr_name ] campaign</code>
+	    			<br/><i>Example URL:</i> <code>http://realbigmarketing.com/?utm_name=viral</code>
+	    			<br/><i>End result:</i> <code>This visitor came from our viral campaign</code>
+        		</td>
+        	</tr>
+    	</tbody>
+    </table>
 </div>
 <?php }
 ?>
--- shortcodes.php
+++ shortcodes.php
@@ -17,9 +17,25 @@ add_shortcode( 'gctr_term', 'gctr_term');
 
 //Replace wrapped text with campaign content
 function gctr_content( $atts, $content = null ) {
-$utm_content = $_REQUEST['utm_campaign'];
+$utm_content = $_REQUEST['utm_content'];
 if (!empty($utm_content)) { return $utm_content; }
 else { return $content; }
 }
 add_shortcode( 'gctr_content', 'gctr_content');
+
+//Replace wrapped text with campaign medium
+function gctr_medium( $atts, $content = null ) {
+$utm_medium = $_REQUEST['utm_medium'];
+if (!empty($utm_medium)) { return $utm_medium; }
+else { return $content; }
+}
+add_shortcode( 'gctr_medium', 'gctr_medium');
+
+//Replace wrapped text with campaign source
+function gctr_source( $atts, $content = null ) {
+$utm_source = $_REQUEST['utm_source'];
+if (!empty($utm_source)) { return $utm_source; }
+else { return $content; }
+}
+add_shortcode( 'gctr_source', 'gctr_source');
 ?>
